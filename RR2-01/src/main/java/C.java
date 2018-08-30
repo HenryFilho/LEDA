19887aa8866a5becf334a4e161e36272640257b4
HenryFilho <henry.filho@ccc.ufcg.edu.br>
2018-08-30 15:10:40 -0300

    asdsadfdfgge

---
+++ RR2-01/src/main/java/C.java
@@ -0,0 +1,15 @@
+import java.util.Collection;
+
+public class C<T> implements A<T> {
+
+	@Override
+	public void m(T t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void m(Collection<?> obj) {
+		//sfdasdfggwegsfehtjryjy
+	}
+	
+}
